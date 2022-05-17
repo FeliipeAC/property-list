65bfd0ead7f8287420d996c424dd5005b32f5953
Felipe Andrade <felipe_fac_8@hotmail.com>
2022-05-16 23:45:12 -0300

    start filter and order list

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,18 +1,139 @@
 import { Component, ReactNode } from "react";
-import FormControl, { useFormControl } from "@mui/material/FormControl";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import Grid from "@mui/material/Grid";
-import InputLabel from "@mui/material/InputLabel";
 
 import "./FilterBar.css";
 
-export class FilterBar extends Component<any> {
+export class FilterBar extends Component<
+  any,
+  {
+    anchorEl: null | HTMLElement;
+    onSearchChange: any;
+    selectedFilter: {
+      key: string;
+      name: string;
+    };
+  }
+> {
+  listOrderOptions = [
+    {
+      key: "price",
+      name: "Lowest price per night",
+    },
+    {
+      key: "rating",
+      name: "Rating",
+    },
+    {
+      key: "name",
+      name: "Name",
+    },
+  ];
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+      onSearchChange: null,
+      selectedFilter: {
+        key: "price",
+        name: "Lowest price per night",
+      },
+    };
+  }
+
+  handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  filterSearch = (value: any) => {
+    // console.log(value.target.value);
+    this.props.onSearchChange(value.target.value);
+  };
+
+  order = (
+    event: React.MouseEvent<HTMLElement>,
+    value: {
+      key: string;
+      name: string;
+    }
+  ) => {
+    console.log(value);
+    this.setState({
+      selectedFilter: value,
+    });
+    this.handleClose();
+  };
+
   render(): ReactNode {
     return (
       <div className="search-container">
-        <Grid container spacing={2}>
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search properties"
+            inputProps={{ "aria-label": "search properties" }}
+          />
+          <SearchIcon />
+          <Divider
+            sx={{ height: 28, m: 0.5 }}
+            orientation="vertical"
+            style={{
+              margin: "0 24px",
+            }}
+          />
+          <div>
+            <Button
+              id="basic-button"
+              aria-controls={!!this.state.anchorEl ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={!!this.state.anchorEl ? "true" : undefined}
+              onClick={this.handleClick}
+            >
+              Order by: {this.state.selectedFilter.name}
+            </Button>
+            <Menu
+              id="basic-menu"
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+              open={!!this.state.anchorEl}
+              anchorEl={this.state.anchorEl}
+              onClose={this.handleClose}
+            >
+              {this.listOrderOptions.map((option, index) => (
+                <MenuItem
+                  key={option.key}
+                  selected={option.key === this.state.selectedFilter.key}
+                  onClick={(event) => this.order(event, option)}
+                >
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </Paper>
+        {/* <Grid container spacing={2}>
           <Grid item xs={9}>
             <div>
               <FormControl
@@ -20,7 +141,10 @@ export class FilterBar extends Component<any> {
                   width: "100%",
                 }}
               >
-                <OutlinedInput placeholder="Search" />
+                <OutlinedInput
+                  placeholder="Search"
+                  onChange={this.filterSearch}
+                />
               </FormControl>
             </div>
           </Grid>
@@ -46,7 +170,7 @@ export class FilterBar extends Component<any> {
               </FormControl>
             </div>
           </Grid>
-        </Grid>
+        </Grid> */}
       </div>
     );
   }
--- src/components/PropertyList/PropertyList.tsx
+++ src/components/PropertyList/PropertyList.tsx
@@ -4,11 +4,15 @@ import Grid from "@mui/material/Grid";
 import { PropertyCard } from "../PropertyCard/PropertyCard";
 import { FilterBar } from "../FilterBar/FilterBar";
 
-export class PropertyList extends Component<any, { list: any[] }> {
+export class PropertyList extends Component<
+  any,
+  { list: any[]; filtredList: any[] }
+> {
   constructor(props: any) {
     super(props);
     this.state = {
       list: [],
+      filtredList: [],
     };
   }
 
@@ -16,21 +20,41 @@ export class PropertyList extends Component<any, { list: any[] }> {
     // Simple GET request using axios
     axios.get("data.json").then((response: any) => {
       console.log("data: ", response.data);
-      this.setState({ list: response.data.properties });
+      this.setState({
+        list: response.data.properties,
+        filtredList: response.data.properties,
+      });
     });
   }
 
+  filter = (value: any) => {
+    console.log(value);
+
+    if (value) {
+      let filtredList = this.state.list.filter((el) =>
+        el.name.toLowerCase().includes(value.toLowerCase())
+      );
+      this.setState({ filtredList: filtredList });
+    } else {
+      this.setState({ filtredList: this.state.list });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <FilterBar />
+      <div
+        style={{
+          padding: "48px 0",
+        }}
+      >
+        <FilterBar onSearchChange={this.filter} />
         <Grid
           container
           spacing={{ xs: 3, md: 6 }}
           justifyContent="flex-start"
           alignItems="stretch"
         >
-          {this.state.list.map((item) => {
+          {this.state.filtredList.map((item) => {
             return (
               <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={item.id}>
                 <PropertyCard item={item} />
