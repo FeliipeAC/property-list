bb3a5010aafe580eaf09332999ef021dac99035b
Felipe Andrade <felipe_fac_8@hotmail.com>
2022-05-19 22:31:58 -0300

    sort by name, price and rating

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -22,162 +22,174 @@ import { styled, alpha } from "@mui/material/styles";
 import "./FilterBar.css";
 
 const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: theme.spacing(1),
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(2),
-    width: "auto",
-  },
+	position: "relative",
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	"&:hover": {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginLeft: theme.spacing(1),
+	width: "100%",
+	[theme.breakpoints.up("sm")]: {
+		marginLeft: theme.spacing(2),
+		width: "auto",
+	},
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+	padding: theme.spacing(0, 2),
+	height: "100%",
+	position: "absolute",
+	pointerEvents: "none",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "16ch",
-      "&:focus": {
-        width: "40ch",
-      },
-    },
-  },
+	color: "inherit",
+	"& .MuiInputBase-input": {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("sm")]: {
+			width: "16ch",
+			"&:focus": {
+				width: "40ch",
+			},
+		},
+	},
 }));
 
 export class FilterBar extends Component<
-  any,
-  {
-    anchorEl: null | HTMLElement;
-    onSearchChange: any;
-    selectedFilter: {
-      key: string;
-      name: string;
-    };
-  }
+	any,
+	{
+		anchorEl: null | HTMLElement;
+		onSearchChange: any;
+		onSortChange: any;
+		selectedFilter: {
+			key: string;
+			name: string;
+		};
+	}
 > {
-  listOrderOptions = [
-    {
-      key: "price",
-      name: "Lowest price per night",
-    },
-    {
-      key: "rating",
-      name: "Rating",
-    },
-    {
-      key: "name",
-      name: "Name",
-    },
-  ];
+	listOrderOptions = [
+		{
+			key: "price",
+			name: "Lowest price per night",
+		},
+		{
+			key: "rating",
+			name: "Rating",
+		},
+		{
+			key: "name",
+			name: "Name",
+		},
+	];
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      anchorEl: null,
-      onSearchChange: null,
-      selectedFilter: {
-        key: "price",
-        name: "Lowest price per night",
-      },
-    };
-  }
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			anchorEl: null,
+			onSearchChange: null,
+			onSortChange: null,
+			selectedFilter: {
+				key: "price",
+				name: "Lowest price per night",
+			},
+		};
+	}
 
-  handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+	handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
 
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-  filterSearch = (value: any) => {
-    // console.log(value.target.value);
-    this.props.onSearchChange(value.target.value);
-  };
+	handleClose = () => {
+		this.setState({ anchorEl: null });
+	};
 
-  order = (
-    event: React.MouseEvent<HTMLElement>,
-    value: {
-      key: string;
-      name: string;
-    }
-  ) => {
-    console.log(value);
-    this.setState({
-      selectedFilter: value,
-    });
-    this.handleClose();
-  };
+	filterSearch = (value: any) => {
+		console.log(value.target.value);
+		this.props.onSearchChange(value.target.value);
+	};
 
-  render(): ReactNode {
-    return (
-      <div className="search-container">
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <div>
-                <IconButton
-                  id="basic-button"
-                  aria-controls={
-                    !!this.state.anchorEl ? "basic-menu" : undefined
-                  }
-                  aria-haspopup="true"
-                  aria-expanded={!!this.state.anchorEl ? "true" : undefined}
-                  onClick={this.handleClick}
-                >
-                  <FilterListIcon />
-                </IconButton>
-                <Menu
-                  id="basic-menu"
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
-                  }}
-                  open={!!this.state.anchorEl}
-                  anchorEl={this.state.anchorEl}
-                  onClose={this.handleClose}
-                >
-                  {this.listOrderOptions.map((option, index) => (
-                    <MenuItem
-                      key={option.key}
-                      selected={option.key === this.state.selectedFilter.key}
-                      onClick={(event) => this.order(event, option)}
-                    >
-                      {option.name}
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </div>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ "aria-label": "search" }}
-                />
-              </Search>
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </div>
-    );
-  }
+	sort = (value: { key: string; name: string }) => {
+		this.setState({
+			selectedFilter: value,
+		});
+		this.props.onSortChange(value);
+		this.handleClose();
+	};
+
+	render(): ReactNode {
+		return (
+			<div className="search-container">
+				<Box sx={{ flexGrow: 1 }}>
+					<AppBar position="static">
+						<Toolbar>
+							<div>
+								<IconButton
+									id="basic-button"
+									aria-controls={
+										!!this.state.anchorEl
+											? "basic-menu"
+											: undefined
+									}
+									aria-haspopup="true"
+									aria-expanded={
+										!!this.state.anchorEl
+											? "true"
+											: undefined
+									}
+									onClick={this.handleClick}
+								>
+									<FilterListIcon />
+								</IconButton>
+								<Menu
+									id="basic-menu"
+									MenuListProps={{
+										"aria-labelledby": "basic-button",
+									}}
+									open={!!this.state.anchorEl}
+									anchorEl={this.state.anchorEl}
+									onClose={this.handleClose}
+								>
+									{this.listOrderOptions.map(
+										(option, index) => (
+											<MenuItem
+												key={option.key}
+												selected={
+													option.key ===
+													this.state.selectedFilter
+														.key
+												}
+												onClick={(event) =>
+													this.sort(option)
+												}
+											>
+												{option.name}
+											</MenuItem>
+										)
+									)}
+								</Menu>
+							</div>
+							<Search>
+								<SearchIconWrapper>
+									<SearchIcon />
+								</SearchIconWrapper>
+								<StyledInputBase
+									placeholder="Search…"
+									inputProps={{ "aria-label": "search" }}
+									onChange={this.filterSearch}
+								/>
+							</Search>
+						</Toolbar>
+					</AppBar>
+				</Box>
+			</div>
+		);
+	}
 }
--- src/components/PropertyList/PropertyList.tsx
+++ src/components/PropertyList/PropertyList.tsx
@@ -5,64 +5,111 @@ import { PropertyCard } from "../PropertyCard/PropertyCard";
 import { FilterBar } from "../FilterBar/FilterBar";
 
 export class PropertyList extends Component<
-  any,
-  { list: any[]; filtredList: any[] }
+	any,
+	{ list: any[]; filtredList: any[] }
 > {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      list: [],
-      filtredList: [],
-    };
-  }
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			list: [],
+			filtredList: [],
+		};
+	}
 
-  componentDidMount() {
-    // Simple GET request using axios
-    axios.get("data.json").then((response: any) => {
-      console.log("data: ", response.data);
-      this.setState({
-        list: response.data.properties,
-        filtredList: response.data.properties,
-      });
-    });
-  }
+	componentDidMount() {
+		// Simple GET request using axios
+		axios.get("data.json").then((response: any) => {
+			console.log("data: ", response.data);
+			this.setState({
+				list: response.data.properties,
+				filtredList: response.data.properties,
+			});
 
-  filter = (value: any) => {
-    console.log(value);
+			this.sort({
+				key: "price",
+				name: "Lowest price per night",
+			});
+		});
+	}
 
-    if (value) {
-      let filtredList = this.state.list.filter((el) =>
-        el.name.toLowerCase().includes(value.toLowerCase())
-      );
-      this.setState({ filtredList: filtredList });
-    } else {
-      this.setState({ filtredList: this.state.list });
-    }
-  };
+	filter = (value: any) => {
+		if (value) {
+			let filtredList = this.state.list.filter((el) =>
+				el.name.toLowerCase().includes(value.toLowerCase())
+			);
+			this.setState({ filtredList: filtredList });
+		} else {
+			this.setState({ filtredList: this.state.list });
+		}
+	};
 
-  render() {
-    return (
-      <div
-        style={{
-          padding: "48px 0",
-        }}
-      >
-        <FilterBar onSearchChange={this.filter} />
-        <Grid
-          container
-          spacing={{ xs: 3, md: 6 }}
-          justifyContent="flex-start"
-          alignItems="stretch"
-        >
-          {this.state.filtredList.map((item) => {
-            return (
-              <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={item.id}>
-                <PropertyCard item={item} />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </div>
-    );
-  }
+	sort = (value: { key: string; name: string }) => {
+		switch (value.key) {
+			case "rating":
+				this.setState({
+					filtredList: this.state.list.sort(
+						(a, b) =>
+							b.overallRating?.overall - a.overallRating?.overall
+					),
+				});
+				break;
+			case "name":
+				this.setState({
+					filtredList: this.state.list.sort((a, b) =>
+						a.name.localeCompare(b.name, "en", {
+							numeric: true,
+						})
+					),
+				});
+				break;
+			case "price":
+				this.setState({
+					filtredList: this.state.list.sort(
+						(a, b) =>
+							a.lowestPricePerNight.value -
+							b.lowestPricePerNight.value
+					),
+				});
+				break;
+			default:
+				break;
+		}
+	};
+
+	render() {
+		return (
+			<div
+				style={{
+					padding: "48px 0",
+				}}
+			>
+				<FilterBar
+					onSearchChange={this.filter}
+					onSortChange={this.sort}
+				/>
+				<Grid
+					container
+					spacing={{ xs: 3, md: 6 }}
+					justifyContent="flex-start"
+					alignItems="stretch"
+				>
+					{this.state.filtredList.map((item) => {
+						return (
+							<Grid
+								item
+								xs={12}
+								sm={6}
+								md={4}
+								lg={4}
+								xl={3}
+								key={item.id}
+							>
+								<PropertyCard item={item} />
+							</Grid>
+						);
+					})}
+				</Grid>
+			</div>
+		);
+	}
 }
